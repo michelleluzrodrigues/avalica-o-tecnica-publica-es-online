23aa6a689beae70317afe641638aa07ce8e6bfe4
michelleluzrodrigues <michellerodrigues268@gmail.com>
2025-02-23 17:04:13 -0300

    Atualização liquidibase

--- src/main/java/com/example/processos/model/Reu.java
+++ src/main/java/com/example/processos/model/Reu.java
@@ -1,11 +1,11 @@
 package com.example.processos.model;
 
-import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
